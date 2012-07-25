8fcdf7d171587d5dbe838c9e9db91a645cd6b10e
Axel Isouard <axel.isouard@epitech.eu>
2012-07-25 12:47:24 +0200

    writing a GLSL caching feature, will create a file for each compiled shader, and automatically remove no longer existing cached shaders

---
+++ shadermodeinfo.h
@@ -0,0 +1,8 @@
+typedef struct shadermodeinfo_s
+{
+  const char *vertexfilename;
+  const char *geometryfilename;
+  const char *fragmentfilename;
+  const char *pretext;
+  const char *name;
+} shadermodeinfo_t;
