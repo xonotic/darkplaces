7530af6562f9c8d479cdffb71e9c570af161d8a5
Axel Isouard <axel.isouard@epitech.eu>
2012-07-26 09:49:44 +0200

    little edit

--- gl_cache.c
+++ gl_cache.c
@@ -1,5 +1,15 @@
 #include "gl_cache.h"
 
+/*
+ * GL_Cache_Init()
+ * Look for each cached file, remove the non existing
+ * cached shaders.
+ */
 void GL_Cache_Init(void)
 {
+  /*  fssearch_t *c, *s;
+  char vabuf[1024];
+
+  va(vabuf, sizeof(vabuf), "%s%s", *fs_userdir ? fs_userdir : fs_basedir, "cache/*.bin");
+  c = FS_Search(vabuf, 1, true);*/
 }
