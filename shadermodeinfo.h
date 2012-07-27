fed221a2c140ee0d1b002ccc01eac33cf0a114c3
Axel Isouard <axel.isouard@anexfi.fr>
2012-07-27 08:06:19 +0200

    fixed a little compiling issue

--- shadermodeinfo.h
+++ shadermodeinfo.h
@@ -1,8 +1,8 @@
-typedef struct shadermodeinfo_s
+struct shadermodeinfo_s
 {
   const char *vertexfilename;
   const char *geometryfilename;
   const char *fragmentfilename;
   const char *pretext;
   const char *name;
-} shadermodeinfo_t;
+};
