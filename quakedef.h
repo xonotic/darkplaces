e5845e0cabff35dc674ad5422799a02c025228f0
Rudolf Polzer <divverent@xonotic.org>
2012-08-01 09:52:49 +0200

    add a #define HARDENING for the file extension checking (more to come)

--- quakedef.h
+++ quakedef.h
@@ -32,6 +32,12 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define DP_FUNC_NORETURN
 #endif
 
+#ifdef HARDENING
+# define IS_HARDENED false
+#else
+# define IS_HARDENED true
+#endif
+
 #ifdef ANTICHEAT
 #define _GNU_SOURCE // for memmem
 #endif
