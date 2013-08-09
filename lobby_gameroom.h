6e80d81741a8534af8d32f2d607167837c356517
Brian Bosak <webadm@elcnet.servehttp.com>
2013-08-09 13:36:30 -0500

    Added missing files

---
+++ lobby_gameroom.h
@@ -0,0 +1,14 @@
+/*
+ * idwmaster_gameroom.h
+ *
+ *  Created on: Aug 7, 2013
+ *      Author: brian
+ */
+
+#ifndef IDWMASTER_GAMEROOM_H_
+#define IDWMASTER_GAMEROOM_H_
+void lobby_Init();
+void lobby_Loop();
+
+
+#endif /* IDWMASTER_GAMEROOM_H_ */
