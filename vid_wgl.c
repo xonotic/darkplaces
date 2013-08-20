50c1a6fa96850f5e5d67090167c38baf43e2937d
Rudolf Polzer <divverent@xonotic.org>
2013-08-20 16:27:54 +0200

    add win32 version of desktopfullscreen

--- vid_wgl.c
+++ vid_wgl.c
@@ -1012,7 +1012,14 @@ qboolean VID_InitModeGL(viddef_mode_t *mode)
 	vid_isfullscreen = false;
 	if (fullscreen)
 	{
-		if(vid_forcerefreshrate.integer)
+		if(vid_desktopfullscreen.integer)
+		{
+			gdevmode = initialdevmode;
+			width = mode->width = gdevmode.dmPelsWidth;
+			height = mode->height = gdevmode.dmPelsHeight;
+			bpp = mode->bpp = gdevmode.dmBitsPerPel;
+		}
+		else if(vid_forcerefreshrate.integer)
 		{
 			foundmode = true;
 			gdevmode.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
