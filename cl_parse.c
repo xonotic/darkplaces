ae3cb741eaffb88a79cc4f4d2c5cea041febaa3e
Cloudwalk <cloudwalk009@gmail.com>
2020-08-21 07:21:23 -0400

    cl_parse: Use Cbuf_InsertText for svc_sellscreen

--- cl_parse.c
+++ cl_parse.c
@@ -3658,7 +3658,7 @@ void CL_ParseServerMessage(void)
 				break;
 
 			case qw_svc_sellscreen:
-				Cmd_ExecuteString(&cmd_client, "help", src_command, true);
+				Cbuf_InsertText(&cmd_client, "help");
 				break;
 
 			case qw_svc_smallkick:
@@ -4165,7 +4165,7 @@ void CL_ParseServerMessage(void)
 				break;
 
 			case svc_sellscreen:
-				Cmd_ExecuteString(&cmd_client, "help", src_command, true);
+				Cbuf_InsertText(&cmd_client, "help");
 				break;
 			case svc_hidelmp:
 				if (gamemode == GAME_TENEBRAE)
