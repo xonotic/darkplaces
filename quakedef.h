dc0feea4cbb683deff849c0fe49a741878667967
Lock l00p <hakzstudios@gmail.com>
2023-10-08 04:40:52 +0000

    Made config not be in data directory

--- quakedef.h
+++ quakedef.h
@@ -29,7 +29,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef __EMSCRIPTEN__
     #define CONFIGFILENAME "config.cfg"
 #else
-    #define CONFIGFILENAME "/data/config.cfg"
+    #define CONFIGFILENAME "/config/config.cfg"
 #endif
 
 // moveflags values
